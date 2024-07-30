2c8c7fa2154af7c44c511d3942a82b7c18401d0b
osalyousef <os.forwork1@gmail.com>
2024-07-30 04:59:01 +0300

    Initial Commit

---
+++ iSaver/iSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSaverApp.swift
+//  iSaver
+//
+//  Created by Osamah alyousef on 30/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct iSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
